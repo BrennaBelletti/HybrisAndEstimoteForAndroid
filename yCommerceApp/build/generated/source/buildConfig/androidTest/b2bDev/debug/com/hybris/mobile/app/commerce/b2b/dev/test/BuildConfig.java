8c92f0a4b841d724da31e6496a85dcaa374a602d
brenna.belletti <brennabelletti@yahoo.com>
2017-04-13 15:49:09 -0400

    Clean hybris (android SDK) commit

---
+++ yCommerceApp/build/generated/source/buildConfig/androidTest/b2bDev/debug/com/hybris/mobile/app/commerce/b2b/dev/test/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.hybris.mobile.app.commerce.b2b.dev.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.hybris.mobile.app.commerce.b2b.dev.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "b2bDev";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+  public static final String FLAVOR_vertical = "b2b";
+  public static final String FLAVOR_server = "dev";
+}
